777097793b54e7b17057f968a12cfa7de03f5651
NurFullS <nurzhenishov10@gmail.com>
2025-09-09 22:16:27 +0600

    start project'

---
+++ src/app/types/user.ts
@@ -0,0 +1,8 @@
+export type User = {
+    username: string;
+    email: string;
+    id: number;
+    password: string;
+    avatar: string;
+    role: string;
+}
---
+++ src/app/types/video.ts
@@ -0,0 +1,5 @@
+export type Video = {
+    video: string;
+    url: string;
+    description: string;
+}
